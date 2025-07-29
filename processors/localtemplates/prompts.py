10bbfdc01cc96a6b62930398487f712e42690136
Carl Petersson <cpetersson@ibm.com>
2025-07-29 12:09:25 -0500

    Refactored repo

--- processors/localtemplates/prompts.py
+++
@@ -1,9 +0,0 @@
-MAIN_PROMPT = """
-You are a copywriter and you have been provided with the following paragraph:
-{context}
-Using the paragraph above, 
-generate 3 questions about the topics discussed in the paragraph, 
-and provide an accurate answer for each question. Do not stray from the information provided in the paragraph.  
-Try not to exceed 2 sentences for each answer.
-Write the output as a JSON list.
-"""
